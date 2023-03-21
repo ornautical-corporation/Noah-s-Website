df7f3de3877aeffd8153b739622a37bc46724a32
ornautical-corporation <128444782+ornautical-corporation@users.noreply.github.com>
2023-03-20 22:21:02 -0500

    Add files via upload

---
+++ noahs webpage hub.js
@@ -0,0 +1,15 @@
+// Declare const variables
+
+const win = window;
+
+// Secret 1 //
+
+function myReset() {
+    document.getElementById("Faq").innerHTML = 100027382738 + 129736271;
+    win.alert("???");
+    console.log("Number Secret Discovered");
+}
+
+//Greeting
+
+// win.alert("Welcome back to the Hub");
